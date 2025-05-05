4b0c3c1481bf469468fad4bb0cf6c843bf099d05
Pavel <boozyweezy@MacBook-Pro-Pavel.local>
2025-05-06 02:25:55 +0300

    fix

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import path    from 'node:path';
 import fs      from 'node:fs';
 import crypto  from 'node:crypto';
 import { fileURLToPath } from 'node:url';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 dotenv.config();
 
@@ -31,6 +32,31 @@ app.use(
 );
 
 // ───────────────────────── MongoDB ─────────────────────────
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 (async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
