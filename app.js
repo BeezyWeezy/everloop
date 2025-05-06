03a1f356110c15da877d8f093fc009e01cc8ee28
Pavel <boozyweezy@MacBook-Pro-Pavel.local>
2025-05-06 03:27:56 +0300

    fix

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+    try {
+        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+        await mongoose.connect(process.env.MONGO_URI, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await mongoose.disconnect();
+    }
+}
+run().catch(console.dir);
+
 /* ───────────────────────── Mongo init ───────────────────────── */
 mongoose.set('strictQuery', true);
 
