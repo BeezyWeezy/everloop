4532f9d68214e9ad37110e1654a54923f73ebdd5
Pavel <boozyweezy@MacBook-Pro-Pavel.local>
2025-05-06 04:42:12 +0300

    fix

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ import path    from 'node:path';
 import fs      from 'node:fs';
 import crypto  from 'node:crypto';
 import { fileURLToPath } from 'node:url';
-import cors from 'cors';
-
 
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
@@ -35,7 +33,6 @@ function checkTelegramHash(data) {
 }
 
 const app = express();
-app.use(cors());
 app.use((req,res,next)=>{console.log('REQ >>>', req.method, req.url); next();});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
